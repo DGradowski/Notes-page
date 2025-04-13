2ab826f3dfcb373113d5b3151d54d289bbb88cae
Dawid Gradowski <251524@edu.p.lodz.pl>
2025-04-13 14:59:15 +0200

    Finish GPIO and add on page

--- docs/sem4/wbudowane/notatka/code.c
+++ docs/sem4/wbudowane/notatka/code.c
@@ -1,19 +1,11 @@
-
-// preferowane
-if if ((GPIO_ReadValue(0) & (1 << 3)) == 0) {
-    // niski sygnał
+if ((GPIO_ReadValue(0) & (1 << 4)) == 0) {
+    // przycisk wciśnięty
 } else {
-    // wysoki sygnał
+    // przycisk nie wciśnięty
 }
 
-if (((GPIO_ReadValue(0) >> 3) & 0x01) == 0) {
-    // niski sygnał
+if ((GPIO_ReadValue(0) & (1 << 4)) > 0) {
+    // przycisk nie wciśnięty
 } else {
-    // wysoki sygnał
-}
-
-if if ((GPIO_ReadValue(0) & 8) == 0) {
-    // niski sygnał
-} else {
-    // wysoki sygnał
+    // przycisk wciśnięty
 }
