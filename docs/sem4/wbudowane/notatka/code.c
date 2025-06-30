114e3549e417c4b3c1b5661d09dc8666f646d3cf
Dawid Gradowski <251524@edu.p.lodz.pl>
2025-06-30 21:04:42 +0200

    Update index.html and add TIPS notes

--- docs/sem4/wbudowane/notatka/code.c
+++ docs/sem4/wbudowane/notatka/code.c
@@ -1,11 +1,24 @@
-if ((GPIO_ReadValue(0) & (1 << 4)) == 0) {
-    // przycisk wciśnięty
-} else {
-    // przycisk nie wciśnięty
-}
+void oled_init (void)
+{
+    int i = 0;
 
-if ((GPIO_ReadValue(0) & (1 << 4)) > 0) {
-    // przycisk nie wciśnięty
-} else {
-    // przycisk wciśnięty
+    // Ustawianie kierunków pinów na wyjście
+    GPIO_SetDir(2, (1<<1), 1); // Zasilanie wyświetlacza
+    GPIO_SetDir(2, (1<<7), 1); // Data/Command
+    GPIO_SetDir(0, (1<<6), 1); // Chip Select
+
+    /* make sure power is off */
+    GPIO_ClearValue( 2, (1<<1) );
+
+    OLED_CS_OFF(); // to samo co GPIO_SetValue( 0, (1<<6) )
+
+    runInitSequence();
+
+    memset(shadowFB, 0, SHADOW_FB_SIZE);
+
+    /* small delay before turning on power */
+    for (i = 0; i < 0xffff; i++);
+
+     /* power on */
+    GPIO_SetValue( 2, (1<<1) );
 }
